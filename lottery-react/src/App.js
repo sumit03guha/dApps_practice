071cbda0047b3137f1eabfcd91677bd1b4725cea
Sumit Guha <sumit03guha@gmail.com>
2021-07-08 22:16:48 +0530

    little useless changes

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,30 +1,55 @@
 // import logo from './logo.svg';
 import './App.css';
-import lotteryAbi from './lottery';
+import lottery from './lottery';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
+
+const ABI = lottery.abi;
+const address = lottery.address;
 
 function App() {
   const [manager , setManager] = useState();
   const [contract , setContract] = useState();
-
+  
+  const loadweb3 = async () => {
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      console.log('Ethereum successfully detected!');
+ 
+  // provider === window.ethereum 
+    
+    const chainId = await provider.request({
+      method: 'eth_chainId'
+    })
+    } else {
+  // if the provider is not detected, detectEthereumProvider resolves to null
+      console.log('Please install MetaMask!')
+    }
+    
+    const web3 = new Web3(provider);
+    console.log(await web3.eth.getAccounts()) //the account shows up fine that will be called to deploy the contract.
+  }
+  
   const loadBlockchain = async () => {
-    // await window.ethereum.send('eth_requestAccounts');
-    window.web3 = new Web3(window.ethereum);
-    const contract1 = await new window.web3.eth.Contract(lotteryAbi , '0xBC3ec9Fd4C34cAA40a934703E825Ee058C2D9570');
-    // console.log(await new window.web3.eth.Contract(lotteryAbi , '0xBC3ec9Fd4C34cAA40a934703E825Ee058C2D9570'));
+    const web3 = new Web3(window.ethereum);
+    const contract1 = await new web3.eth.Contract(ABI , address);
     setContract(contract1);
-    console.log("blockchain")
+    console.log(await new web3.eth.Contract(ABI , address).methods);
+    // the methods are showing up perfectly but whenever I access the getManager as shown in the managerAddress below it gives the error as shown
+    // in the image.
+    console.log("contract loaded");
   } 
 
   const managerAddress = async () => {
-    const manager1 = await contract.methods.getManager().call();
+    const manager1 = await contract.methods.getManager().call(); // THIS IS WHERE THE ERROR OCCURS.
     setManager(manager1); 
-    console.log("man");  
+    console.log(manager);  
   }
 
   useEffect(()=>{
+    loadweb3();
     loadBlockchain();
     console.log('1');    
     managerAddress();
--- lottery-react/src/lottery.js
+++ lottery-react/src/lottery.js
@@ -1,64 +1,66 @@
-const abi = [
-  {
-    inputs: [],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
-    constant: undefined,
-    payable: undefined,
-    signature: 'constructor'
-  },
-  {
-    inputs: [],
-    name: 'getManager',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true,
-    payable: undefined,
-    signature: '0xd5009584'
-  },
-  {
-    inputs: [],
-    name: 'getPlayers',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true,
-    payable: undefined,
-    signature: '0x8b5b9ccc'
-  },
-  {
-    inputs: [],
-    name: 'pickWinner',
-    outputs: [ [Object] ],
-    stateMutability: 'nonpayable',
-    type: 'function',
-    constant: undefined,
-    payable: undefined,
-    signature: '0x5d495aea'
-  },
-  {
-    inputs: [ [Object] ],
-    name: 'players',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true,
-    payable: undefined,
-    signature: '0xf71d96cb'
-  },
-  {
-    inputs: [],
-    name: 'register',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
-    constant: undefined,
-    payable: true,
-    signature: '0x1aa3a008'
-  }
-];
+const lottery = {
+  abi : [
+    {
+      inputs: [],
+      stateMutability: 'nonpayable',
+      type: 'constructor',
+      constant: undefined,
+      payable: undefined,
+      signature: 'constructor'
+    },
+    {
+      inputs: [],
+      name: 'getManager',
+      outputs: [ [Object] ],
+      stateMutability: 'view',
+      type: 'function',
+      constant: true,
+      payable: undefined,
+      signature: '0xd5009584'
+    },
+    {
+      inputs: [],
+      name: 'getPlayers',
+      outputs: [ [Object] ],
+      stateMutability: 'view',
+      type: 'function',
+      constant: true,
+      payable: undefined,
+      signature: '0x8b5b9ccc'
+    },
+    {
+      inputs: [],
+      name: 'pickWinner',
+      outputs: [ [Object] ],
+      stateMutability: 'nonpayable',
+      type: 'function',
+      constant: undefined,
+      payable: undefined,
+      signature: '0x5d495aea'
+    },
+    {
+      inputs: [ [Object] ],
+      name: 'players',
+      outputs: [ [Object] ],
+      stateMutability: 'view',
+      type: 'function',
+      constant: true,
+      payable: undefined,
+      signature: '0xf71d96cb'
+    },
+    {
+      inputs: [],
+      name: 'register',
+      outputs: [],
+      stateMutability: 'payable',
+      type: 'function',
+      constant: undefined,
+      payable: true,
+      signature: '0x1aa3a008'
+    }
+  ],
 
-// const address = '0xD9E0D184D2F356Abe6465CABcd6B8F67A38835b2';
+  address : '0xD9E0D184D2F356Abe6465CABcd6B8F67A38835b2'
+}
 
-export default abi;
+export default lottery;
